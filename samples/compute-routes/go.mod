c85c43231ebae34b28be4025fa4944a67f49c2f2
Justin <jpoehnelt@google.com>
2020-04-29 12:13:56 -0700

    docs: fix sample

--- samples/compute-routes/go.mod
+++ samples/compute-routes/go.mod
@@ -3,6 +3,7 @@ module main
 go 1.14
 
 require (
-	developers.google.com/maps/go/routespreferred v0.0.0-20200415173922-fa4d558f5bbd
-	google.golang.org/genproto v0.0.0-20200417142217-fb6d0575620b
+	developers.google.com/maps/go/routespreferred v0.0.0-20200422232245-653457bbb2a2
+	google.golang.org/genproto v0.0.0-20200429120912-1f37eeb960b2
+	google.golang.org/grpc v1.29.1
 )
--- samples/compute-routes/main.go
+++ samples/compute-routes/main.go
@@ -5,17 +5,21 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
-	"developers.google.com/maps/go/routespreferred/v1"
+	routespreferred "developers.google.com/maps/go/routespreferred/v1"
 	routespb "google.golang.org/genproto/googleapis/maps/routes/v1"
 	"google.golang.org/genproto/googleapis/type/latlng"
+	"google.golang.org/grpc/metadata"
 )
 
 func main() {
-	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 
-	// instantiate client with default credentials
-	c, err := routes.NewRoutesPreferredClient(ctx)
+	// instantiate a client
+	c, err := routespreferred.NewRoutesPreferredClient(ctx)
+	defer c.Close()
 
 	if err != nil {
 		log.Fatal(err)
@@ -62,13 +66,23 @@ func main() {
 		PolylineQuality: routespb.PolylineQuality_OVERVIEW,
 	}
 
+	// add an API key to the request
+	ctx = metadata.AppendToOutgoingContext(ctx, "X-Goog-Api-Key", "YOUR KEY")
+
+	// set the field mask
+	ctx = metadata.AppendToOutgoingContext(ctx, "X-Goog-Fieldmask", "*")
+
 	// execute rpc
 	resp, err := c.ComputeRoutes(ctx, req)
 
 	if err != nil {
+		// "rpc error: code = InvalidArgument desc = Request contains an invalid
+		// argument" may indicate an invalid API key or project not having
+		// access to Routes Preferred
 		log.Fatal(err)
 	}
 
 	fmt.Printf("Duration of route %d", resp.Routes[0].Duration.Seconds)
 }
+
 // [END maps_routes_preferred_compute_routes]
